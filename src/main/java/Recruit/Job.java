16cb1d9beca7f108951318710f7d84fb8db47f2a
moonhyuk <godmoon00@naver.com>
2020-12-07 22:49:26 +0900

    Improve RecruitAbstract

---
+++ src/main/java/Recruit/Job.java
@@ -0,0 +1,13 @@
+package Recruit;
+
+public class Job {
+    public String subject;
+    public String startDate;
+    public String endDate;
+
+    public Job (String subject, String startDate, String endDate) {
+        this.subject = subject;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
---
+++ src/main/java/Recruit/RecruitFactory.java
@@ -0,0 +1,8 @@
+package Recruit;
+
+public class RecruitFactory {
+    public static RecruitAbstract make(String type)
+    {
+        return new SNURecruit(new Page());
+    }
+}
